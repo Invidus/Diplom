c4dd4f39c09310164079612eede5d5684ff0b7d7
Dmitriy Buyanov <demon.cc@yandex.ru>
2019-02-10 20:21:42 +0300

    index to show
    index to show

---
+++ Show/nav.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Page Title</title>
+  <link rel="stylesheet" type="text/css" media="screen" href="Style.css" />
+</head>
+<body>
+  
+<nav class="navbar navbar-expand-lg navbar-light ">
+            <a class="navbar-brand brand-logo" href="index.php"><img class = "logo" src="images/HLogo.png" alt="logo" width="100px" height="87px"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <a class="nav-link" href="index.php">Главная </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="calculate.php">Расчет калорий</a>
+                </li>
+                 <li class="nav-item">
+                  <a class="nav-link" href="news.php">Новости</a>
+                </li> 
+                
+                <!-- <div id="slow_nav">
+                <li>
+                    <a class="nav-link" href="#">пункт 1</a>
+                    <ul>
+                        <li class="nav-item"><a class="nav-link" href="#">Выпадающий пункт 1</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Выпадающий пункт 2</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Выпадающий пункт 3</a></li>
+                    </ul>
+                  </li>
+                  </div> 
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Продукция
+                  </a> 
+                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                  </div>
+                </li>              -->
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Поддержка</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#contacts">Контакты</a>
+                </li>
+              </ul>
+              <div class="box">
+                <div class="container-4">
+                  <input type="search" id="search" placeholder="Поиск..." />
+                  <button class="icon"><i class="fa fa-search"></i></button>
+                </div>
+              </div>
+            </div>            
+          </nav></body>
+</html>
